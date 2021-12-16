2ed4c78b551b1b1a9cb074752734b33d61f4a59e
MarlinDayana <florezmarlin1996@gmail.com>
2021-12-16 09:50:59 -0500

    webserver listo

---
+++ app-old.js
@@ -0,0 +1,12 @@
+// const http = require('http');
+
+
+// http.createServer((req, res)=> { // create server es para crear un servidor con argumentos de solicitud y respuestas 
+//     res.write('hello world');   
+//     res.end();
+// })  // importante recordar que las instruccuines que se hagan en un response siempre deben terminar con res.end();
+
+
+// .listen( 8080);// listen es para indicar por cualpuerto se va a mostrar la información
+
+// console.log('listening on port 8080');
